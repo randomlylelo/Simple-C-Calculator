cb582f75c47abac557b077cf9a9b0ccd3ec99140
Cretilo <38267386+cretilo@users.noreply.github.com>
2018-06-05 16:10:49 -0400

    removed useless variable & added error code for '0' divisions

--- program-here/hello.cpp
+++ program-here/hello.cpp
@@ -13,60 +13,62 @@ int main ()
     string correction;
     string username;
     string password;
-    string auth;
     cout <<"Please login" << "\n" << "Username: ";
     cin >> username;
     cout <<"Password: ";
     cin >> password;
     if (username == "root" && password == "root")
     {
-        auth = "true";
         cout <<"Access Granted..." <<"\n";
-        if(auth == "true")
+        cout <<"Please choose what operation you would like to do\nNOTE PLEASE TYPE the word not the symbol\naddition(+)\nsubtraction(-)\nmuliplication(*)\ndivision(/)\n";
+        cin >> operation;
+        cout <<"Please choose a number: " << "\n";
+        cin >> fir_num;
+        cout <<"Please choose another number: " << "\n";
+        cin >> sec_num;
+        cout <<"Your first number is: " << fir_num << "\n" <<"Your second number is: " << sec_num << "\n" <<"Your choice of operation is: " << operation << "\n";
+        cout <<"Is this correct? (answer with 'yes' or 'no')" << "\n";
+        cin >> correct;
+        cout <<"\n";
+        if (correct == "yes")
         {
-            cout <<"Please choose what operation you would like to do\nNOTE PLEASE TYPE the word not the symbol\naddition(+)\nsubtraction(-)\nmuliplication(*)\ndivision(/)\n";
-            cin >> operation;
-            cout <<"Please choose a number: " << "\n";
-            cin >> fir_num;
-            cout <<"Please choose another number: " << "\n";
-            cin >> sec_num;
-            cout <<"Your first number is: " << fir_num << "\n" <<"Your second number is: " << sec_num << "\n" <<"Your choice of operation is: " << operation << "\n";
-            cout <<"Is this correct? (answer with 'yes' or 'no')" << "\n";
-            cin >> correct;
-            cout <<"\n";
-            if (correct == "yes")
+            if (operation == "addition")
             {
-                if (operation == "addition")
-                {
-                    cout << fir_num << " + " << sec_num << " = " << fir_num + sec_num << "\n";
-                }
-                if (operation == "subtraction")
-                {
-                    cout << fir_num << " - " << sec_num << " = " << fir_num - sec_num << "\n";
-                }
-                if (operation == "muliplication")
-                {
-                    cout << fir_num << " * " << sec_num << " = " << fir_num * sec_num << "\n";
-                }
-                if (sec_num != 0 && operation == "division")
+                cout << fir_num << " + " << sec_num << " = " << fir_num + sec_num << "\n";
+            }
+            if (operation == "subtraction")
+            {
+                cout << fir_num << " - " << sec_num << " = " << fir_num - sec_num << "\n";
+            }
+            if (operation == "muliplication")
+            {
+                cout << fir_num << " * " << sec_num << " = " << fir_num * sec_num << "\n";
+            }
+            if (operation == "division")
+            {
+                if (sec_num != 0)
                 {
                     cout << fir_num << " / " << sec_num << " = " << fir_num / sec_num << "\n";
                 }
-                cout <<"\n";
+                else
+                {
+                    cout <<"Please restart the program and not put '0' as the second number! Thanks." << "\n";
+                }
+            }
+            cout <<"\n";
+            return 0;
+        }
+        if (correct == "no")
+        {
+            cout <<"Would you like to contiune and retype everything? (answer with 'yes' or 'no')" <<"\n";
+            cin >> correction;
+            if (correction == "no")
+            {
                 return 0;
             }
-            if (correct == "no")
-            {
-                cout <<"Would you like to contiune and retype everything? (answer with 'yes' or 'no')" <<"\n";
-                cin >> correction;
-                if (correction == "no")
-                {
-                    return 0;
-                }
-                cout <<"Restarting the program..." << "\n";
-            }
-            
+            cout <<"Restarting the program..." << "\n";
         }
+            
     }
         else
     {
